549b2440eda3f6d2482c0725a2780be09900cb4d
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-10-09 06:48:45 -0400

    Descending order printing works. Problem with words with apostrophes and em/en dashes. Encoding error with the-hill-we-climb test file

--- word_frequency.py
+++ word_frequency.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 import string
-
+from collections import Counter
 
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has','he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were', 'will', 'with' # noqa
@@ -10,12 +11,20 @@ def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     with open(file) as target:
         txt_string = target.read()
-        print(txt_string)
-        #txt_string = txt_string.translate(str.maketrans('', '', string.punctuation)).casefold() # noqa
+        txt_string = txt_string.strip()
+        txt_string = txt_string.translate(str.maketrans('', '', string.punctuation)).casefold() # noqa
+        txt_list = txt_string.split()
 
-
-
-        #print(txt_string)
+        new_list = [word for word in txt_list if word not in STOP_WORDS]
+        counts = Counter(new_list)
+        count_ordered = dict(sorted(counts.items(), key=lambda x: x[1], reverse=True)) # noqa
+        for key, value in count_ordered.items():
+            counter_mark = ''
+            i = 0
+            while i < value:
+                counter_mark += '*'
+                i += 1
+            print(f'{key} |'.rjust(20), value, counter_mark)
 
 
 if __name__ == "__main__":
