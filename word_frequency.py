48732510ca088bdf4a7f19df9bf9b0e79600106b
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-10-09 04:29:17 -0400

    Filtered punction (em dash is still a problem). Lowercased all words.

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,21 @@
+import string
+
+
 STOP_WORDS = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
-    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has','he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were', 'will', 'with' # noqa
 ]
 
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file) as target:
+        txt_string = target.read()
+        print(txt_string)
+        #txt_string = txt_string.translate(str.maketrans('', '', string.punctuation)).casefold() # noqa
+
+
+
+        #print(txt_string)
 
 
 if __name__ == "__main__":
